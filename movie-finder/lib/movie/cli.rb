e28ecffd763a1690bb077ff0712ecd91a61924cc
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-02 21:46:43 +0000

    Done.

---
+++ movie-finder/lib/movie/cli.rb
@@ -0,0 +1,34 @@
+class MovieFinder::CLI
+
+  def call
+    puts "Welcome to Movie Finder, where we help you out when you run out of ideas on what to watch"
+    start
+  end
+
+  def start
+    puts "What kind of mood are you looking for?"
+    puts "We got all the main ones"
+    puts "You name it"
+    puts "Actually, let me list you our list of genres"
+    puts "Action, of course"
+    puts "Comedy, obviously"
+    puts "Documentary, for the intellectuals"
+    puts "Drama, aka boring or tearjerkers"
+    puts "Horror, if you like"
+    puts "Family, if you must"
+    puts "Mystery, my favorite"
+    puts "Romance, to really set the mood"
+    puts "Science Fiction, because Harry Potter"
+
+    #Top 100 Action
+    #Top 100 Comedy Movies
+    #Top 100 Documentary Movies
+    #Top 100 Drama Movies
+    #Top 100 Horror Movies
+    #Top 100 Family Movies
+    #Top 100 Mystery
+    #Top 100 Romance Movies
+    #Top 100 Science Fiction
+
+
+end
