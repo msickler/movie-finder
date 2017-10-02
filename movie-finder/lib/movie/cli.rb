ec6f17e08f540b1707d092ec9b4d3db281617157
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-02 21:51:01 +0000

    Done.

--- movie-finder/lib/movie/cli.rb
+++ movie-finder/lib/movie/cli.rb
@@ -6,6 +6,7 @@ class MovieFinder::CLI
   end
 
   def start
+    #we want to lighten the mood. i might need to make an until loop here... not yet though
     puts "What kind of mood are you looking for?"
     puts "We got all the main ones"
     puts "You name it"
@@ -19,16 +20,55 @@ class MovieFinder::CLI
     puts "Mystery, my favorite"
     puts "Romance, to really set the mood"
     puts "Science Fiction, because Harry Potter"
+    input = gets.strip
+    if input == "Action"
+      list_action
+    elsif input == "Comedy"
+      list_comedy
+    elsif input == "Documentary"
+      list_documentary
+    elsif input == "Drama"
+      list_drama
+    elsif input == "Horror"
+      list_horror
+    elsif input == "Family"
+      list_family
+    elsif input == "Mystery"
+      list_mystery
+    elsif input == "Romance"
+      list_romance
+    elsif input == "Science Fiction"
+      list_science_fiction
+    else
+    puts "Now now, that's not a genre we mentioned."
+    input = gets.strip
+    if input == "Action"
+      list_action
+    elsif input == "Comedy"
+      list_comedy
+    elsif input == "Documentary"
+      list_documentary
+    elsif input == "Drama"
+      list_drama
+    elsif input == "Horror"
+      list_horror
+    elsif input == "Family"
+      list_family
+    elsif input == "Mystery"
+      list_mystery
+    elsif input == "Romance"
+      list_romance
+    elsif input == "Science Fiction"
+      list_science_fiction
+    else
+      puts "We can wait here all day until you pick a real genre"
+      puts "... That we carry."
+      puts "Here goes the list again: Action, Comedy, Documentary, Drama, Horror, Family, Mystery, Romance and Science Fiction"
+      input = gets.strip
+    end
+  end
+end
 
-    #Top 100 Action
-    #Top 100 Comedy Movies
-    #Top 100 Documentary Movies
-    #Top 100 Drama Movies
-    #Top 100 Horror Movies
-    #Top 100 Family Movies
-    #Top 100 Mystery
-    #Top 100 Romance Movies
-    #Top 100 Science Fiction
 
 
 end
---
+++ movie-finder/lib/movie/genre.rb
@@ -0,0 +1,8 @@
+class Genre
+  attr_accessor :name, :year, :rating, :genre
+
+  def initialize
+
+
+
+end
