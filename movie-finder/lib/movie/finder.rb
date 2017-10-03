a721b0655a237128d0ea9ab060576b2eead5d395
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-03 02:44:17 +0000

    Done.

--- movie-finder/lib/movie/finder.rb
+++ movie-finder/lib/movie/finder.rb
@@ -1,12 +1,11 @@
 require "movie/finder/version"
 #this is after getting an input in which genre to find from. need to make a cli class
 class Finder
-  attr_accessor :name, :year, :rating, :description
+  attr_accessor :name, :rating, :description
   @@all = []
 
-  def initialize(name = nil, year = nil, rating = nil, description = nil)
+  def initialize(name = nil, rating = nil, description = nil)
     @name = name
-    @year = year
     @rating = rating
     @description = description
     @@all << self
@@ -24,24 +23,20 @@ class Finder
     self.all.find_all { |n| n.name == name  }
   end
 
-  def self.find_by_year(year)
-    #users can seach by year
-    self.all.find_all{ |n| n.year == year  }
-  end
-
-  def self.find_by_rating(rating)
+    def self.find_by_rating(rating)
     #users can search by rating
     self.all.find_all { |n| n.rating == rating  }
   end
 
-  def self.random
+  def self.random(genre)
     #users can ask for a random name in a selected genre
+      list = []
+      titles = Scraper.get_page_by_genre.css("a .unstyled-articleLink").text
+      list << titles
+      random_movie = list[rand(list.length)]
+      random_movie
   end
 
-  def self.find_by_year_and_rating(year, rating)
-    self.all.where(year: year, rating: rating)
-    #this would be really cool if i make it work
-  end
 
 
 end
