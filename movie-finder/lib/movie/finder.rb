8ec0f4f19d30c7767283fcf7dcbd429d4e40688e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-03 12:59:22 +0000

    Done.

--- movie-finder/lib/movie/finder.rb
+++ movie-finder/lib/movie/finder.rb
@@ -1,37 +1,35 @@
 require "movie/finder/version"
+
 #this is after getting an input in which genre to find from. need to make a cli class
-class Finder
-  attr_accessor :name, :rating, :genre
+#update : Finder isnt needed by itself, I'm merging it with Genre
+#class Finder
+  #attr_accessor :name, :rating, :genre
   @@all = []
 
-  def initialize(name = nil, rating = nil)
-    @name = name
-    @rating = rating
-    @@all << self
-  end
+  #def initialize(name = nil, rating = nil)
+    #@name = name
+    #@rating = rating
+    #@@all << self
+  #end
 
-  def self.find_by_name(name)
-    list = Genre.print_genre_list
-    list.find_all { |n| n.name == name }
+  #def self.find_by_name(name)
+    #list = Genre.print_genre_list
+    #list.find_all { |n| n.name == name }
     #users can search by name
     #only searching in an already given genre
-  end
+  #end
 
-    def self.find_by_rating(rating)
-      list = Genre.print_list_by_rating
-      list.find_all { |n| n.rating == rating  }
+    #def self.find_by_rating(rating)
+    #  list = Genre.print_list_by_rating
+    #  list.find_all { |n| n.rating == rating  }
     #users can search by rating
-  end
+  #end
 
-  def self.random(genre)
+  #def self.random(genre)
     #users can ask for a random name in a selected genre
-      list = []
-      titles = Genre.print_genre_list(genre)
-      list << titles
-      random_movie = list[rand(list.length)]
-      random_movie
-  end
-
-
-
-end
+  #    list = []
+  #    titles = Genre.print_genre_list(genre)
+  #    list << titles
+  #    random_movie = list[rand(list.length)]
+  #    random_movie
+  #end
