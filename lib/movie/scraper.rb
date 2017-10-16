7310ff81275f4f68a922aee08d42d3e0981ddc92
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-16 16:33:02 +0000

    making movies an array of hashes

--- lib/movie/scraper.rb
+++ lib/movie/scraper.rb
@@ -1,43 +1,53 @@
 class Scraper
-    attr_accessor :genre, :movie
+    attr_accessor :genre, :movie, :doc
     #BASE_URL = 'https://www.rottentomatoes.com/'
 
     def initialize(genre= nil, movie= nil)
       @genre = genre
       @movie = movie
+      @doc = doc
     end
 
     def get_page_by_genre(genre)
       if @genre == "action"
         @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/"))
       elsif genre == "comedy"
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_comedy_movies/"))
+        @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_comedy_movies/"))
       elsif genre == "documentary"
-          doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_documentary_movies/"))
+          @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_documentary_movies/"))
       elsif genre == "drama"
-          doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_drama_movies/"))
+          @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_drama_movies/"))
       elsif genre == "horror"
-          doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_horror_movies/"))
+          @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_horror_movies/"))
       elsif genre == "family"
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_kids__family_movies/"))
+        @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_kids__family_movies/"))
       elsif genre == "mystery"
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_mystery__suspense_movies/"))
+        @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_mystery__suspense_movies/"))
       elsif genre == "romance"
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_romance_movies/"))
+        @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_romance_movies/"))
       else genre == "fantasy"
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_science_fiction__fantasy_movies/"))
+        @doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_science_fiction__fantasy_movies/"))
       end
+      @doc
     end
 
     def new_with_attributes(doc)
       #self.get_page_by_genre(doc).css(".table tr").drop(1).each do |row|
-        new = @doc.search(".table tr").drop(1).each do |row|
-        movie = Movie.new
-        movie.rating = row.css(".tMeterScore").text.gsub("%", "")
-        movie.title = row.css("a.unstyled-articleLink").text
-        movie.link = row.css("a.unstyled-articleLink").attribute("href").value.sub("/", "")
+        movies = []
+        movies_hash = {}
+        movie = @doc.css(".table tr").each do |row|
+          movies_hash = {
+            :title => row.css("a.unstyled-articleLink").text,
+            :rating => row.css(".tMeterScore").text.gsub("%", ""),
+            :link => row.css("a.unstyled-articleLink").attribute("href").value
+          }
+          movies << movies_hash
+        #movie.rating = row.css(".tMeterScore").text.gsub("%", "")
+        #movie.title = row.css("a.unstyled-articleLink").text
+        #movie.link = row.css("a.unstyled-articleLink").attribute("href").value#.sub("/", "")
       end
-      Movie.all
+      movies
+    end
       #self.get_page_by_genre.css(".table tr").drop(1).each do |row|
         #movie = {
         #  :title => row.css("a.unstyled-articleLink").text,
@@ -46,8 +56,6 @@ class Scraper
         #   }
 
         # end
-
-    end
 
     def get_synopsis(movie)
       link = 'https://www.rottentomatoes.com/' + movie.link
